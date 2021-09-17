4b8f58a07a998a1781448d7e9bbee520df919a3e
Mark Lacerenze <marklacer@gmail.com>
2021-09-16 21:16:51 -0300

    Agregando nueva carpeta

---
+++ folder/main.py
@@ -0,0 +1,20 @@
+# quiero sumar dos numeros
+def sumar(a, b):
+  return a + b
+
+# restar dos numeros
+def restar(a, b):
+  return a - b
+
+# multiplicar dos numeros
+def multiplicar(a, b):
+  return a * b
+
+# dividir dos numeros
+def dividir(a, b):
+  return a / b
+
+sumar(1, 2)
+restar(4, 2)
+multiplicar(6, 3)
+dividir(10, 2)
